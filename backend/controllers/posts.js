f61a6e5f94ce156c4408a8c89a18e5f3791ed3bd
= <reeta.trickywebsolutions@gmail.com>
2023-01-19 22:19:32 +0530

    added routes

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,21 +1,78 @@
 const Post = require("../models/posts");
+const User = require("../models/user");
+
 
 exports.create_post = (req, res, next) => {
     const post = new Post(req.body);
     post
-      .save()
-      .then(result => {
-        console.log(result);
-        res.status(201).json({
-          message: "Created product successfully",
-          createdProduct: {
-           post,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/products/" + result._id
-            }
-          }
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: "Created product successfully",
+                createdProduct: {
+                    post,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:3000/products/" + result._id
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
         });
+};
+
+exports.update_posts = async (req, res, next) => {
+    const post = await Post.findById(req.params.id);
+   console.log(post.userId, req.body.userId)
+   if(post.userId === req.body.userId){
+    Post.updateOne({ $set: req.body })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Post updated",
+        post: result,
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/products/" 
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+   }else{
+    res.status(403).json("you can update only your post");
+   }
+    
+};
+
+
+exports.get_all_posts = (req, res, next) => {
+    Post.find()
+      .exec()
+      .then(docs => {
+        const response = {
+          count: docs.length,
+          products: docs.map(doc => {
+            return {
+              post: doc,
+              request: {
+                type: "GET",
+                url: "http://localhost:3000/products/" + doc._id
+              }
+            };
+          })
+        };
+        res.status(200).json(response);
       })
       .catch(err => {
         console.log(err);
@@ -23,4 +80,81 @@ exports.create_post = (req, res, next) => {
           error: err
         });
       });
-  };
+};
+
+
+
+exports.delete_post = async (req, res, next) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        console.log(post.userId, req.body.userId)
+        if (post.userId === req.body.userId) {
+        Post.deleteOne()
+          .exec()
+          .then(result => {
+            console.log(result)
+            res.status(200).json({
+              message: "Post deleted",
+              request: {
+                type: "POST",
+                url: "http://localhost:3000/products",
+              }
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({
+              error: err
+            });
+          });
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+    
+};
+
+
+exports.update_post_like = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+          await post.updateOne({ $push: { likes: req.body.userId } });
+          res.status(200).json("The post has been liked");
+        } else {
+          await post.updateOne({ $pull: { likes: req.body.userId } });
+          res.status(200).json("The post has been disliked");
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+};
+
+
+exports.get_single_post = (req, res, next) => {
+    const id = req.params.id;
+    Post.findById(id)
+      .exec()
+      .then(doc => {
+        console.log("From database", doc);
+        if (doc) {
+          res.status(200).json({
+            post: doc,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products"
+            }
+          });
+        } else {
+          res
+            .status(404)
+            .json({ message: "No valid entry found for provided ID" });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+};
+
+
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -5,7 +5,11 @@ const userController = require('../controllers/posts');
 const checkAuth = require('../middleware/check-auth');
 
 router.post("/", userController.create_post);
-// router.post("/login", userController.user_login);
+router.put("/:id", userController.update_posts);
+router.delete("/:id", userController.delete_post);
+router.get("/", userController.get_all_posts);
+router.put("/:id/like", userController.update_post_like);
+router.get("/:id", userController.get_single_post);
 
 
 module.exports = router;
