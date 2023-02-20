1f2aaacabbb63816e9883b36e8c9a5a00c986540
= <reeta.trickywebsolutions@gmail.com>
2023-02-20 07:18:45 +0530

    final

--- facebook-clone/src/app/shared/store/actions/action-type.ts
+++
@@ -1,3 +0,0 @@
-import * as AuthActions from './auth.action';
-
-export { AuthActions }; // Grouping actions together
--- facebook-clone/src/app/shared/store/actions/course.action.ts
+++
@@ -1,9 +0,0 @@
-import { createAction } from "@ngrx/store";
-
-export const loadAllCourses = createAction(
-    "[Courses Resolver] Load All Courses"
-);
-
-export const allCoursesLoaded = createAction(
-    "[Load Courses Effect] All Courses Loaded"
-);
